b21f970b01ad71af24293766015f2007b7c2d335
Kirill Shatskiy <shaackiy@gmail.com>
2018-01-05 11:20:19 +0300

    Fix reset-css cascading

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 import 'reset-css/reset.css';
+import './index.css';
 
 ReactDOM.render(
   <App />,
