e299a40f97e9af5cd31a97f4bb2b0ff42df23a32
Kirill Shatskiy <shaackiy@gmail.com>
2018-01-06 10:17:22 +0300

    Update Button styles

--- stories/Button.tsx
+++ stories/Button.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
 import Button from '../src/components/Button';
 
 storiesOf('Button', module)
