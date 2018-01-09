6588b57dcd2bb91368fb9bae17579a8a60de3ca5
Kirill Shatskiy <shaackiy@gmail.com>
2018-01-09 19:54:46 +0300

    Add mock data

--- stories/MainPage.tsx
+++ stories/MainPage.tsx
@@ -9,18 +9,70 @@ const roomGroups = [
       {
         title: 'Ржавый Фред',
         description: '3 - 6 человек',
+        events: [
+          {
+            title: 'Событие 1',
+            dateStart: new Date('2018-01-09T06:30:00.55'),
+            dateEnd: new Date('2018-01-09T08:45:00.55')
+          },
+          {
+            title: 'Событие 2',
+            dateStart: new Date('2018-01-09T09:30:00.55'),
+            dateEnd: new Date('2018-01-09T11:45:00.55')
+          },
+          {
+            title: 'Событие 3',
+            dateStart: new Date('2018-01-09T18:30:00.55'),
+            dateEnd: new Date('2018-01-09T20:45:00.55')
+          }
+        ]
       },
       {
         title: 'Прачечная',
         description: 'до 10 человек',
+        events: [
+          {
+            title: 'Событие 3',
+            dateStart: new Date('2018-01-09T5:04:00.55'),
+            dateEnd: new Date('2018-01-09T10::00.55')
+          },
+          {
+            title: 'Событие 4',
+            dateStart: new Date('2018-01-09T14:10:00.55'),
+            dateEnd: new Date('2018-01-09T14:45:00.55')
+          },
+          {
+            title: 'Событие 5',
+            dateStart: new Date('2018-01-09T17:00:00.55'),
+            dateEnd: new Date('2018-01-09T18:45:00.55')
+          }
+        ]
       },
       {
         title: 'Желтый дом',
         description: 'до 10 человек',
+        events: []
       },
       {
         title: 'Оранжевый Тюльпан',
         description: 'до 10 человек',
+        events: [
+          {
+            title: 'Событие 7',
+            dateStart: new Date('2018-01-09T10:04:00.55'),
+            dateEnd: new Date('2018-01-09T13::00.55')
+          },
+          {
+            title: 'Событие 8',
+            dateStart: new Date('2018-01-09T20:00.55'),
+            dateEnd: new Date('2018-01-09T21:00:00.55')
+          },
+          {
+            title: 'Событие 9',
+            dateStart: new Date('2018-01-09T15:00:00.55'),
+            dateEnd: new Date('2018-01-09T16:15:00.55')
+          }
+        ]
       },
     ]
   },
@@ -30,100 +82,62 @@ const roomGroups = [
       {
         title: 'Джокер',
         description: '3 - 6 человек',
+        events: []
       },
       {
         title: 'Мариванна',
         description: '3 - 6 человек',
+        events: [
+          {
+            title: 'Событие 7',
+            dateStart: new Date('2018-01-09T10:04:00.55'),
+            dateEnd: new Date('2018-01-09T13::00.55')
+          },
+          {
+            title: 'Событие 8',
+            dateStart: new Date('2018-01-09T20:00.55'),
+            dateEnd: new Date('2018-01-09T21:00:00.55')
+          },
+          {
+            title: 'Событие 9',
+            dateStart: new Date('2018-01-09T15:00:00.55'),
+            dateEnd: new Date('2018-01-09T16:15:00.55')
+          }
+        ]
       },
       {
         title: 'Тонкий боб',
         description: '3 - 6 человек',
+        events: [
+          {
+            title: 'Событие 1',
+            dateStart: new Date('2018-01-09T06:30:00.55'),
+            dateEnd: new Date('2018-01-09T08:45:00.55')
+          },
+          {
+            title: 'Событие 2',
+            dateStart: new Date('2018-01-09T09:30:00.55'),
+            dateEnd: new Date('2018-01-09T11:45:00.55')
+          },
+          {
+            title: 'Событие 3',
+            dateStart: new Date('2018-01-09T18:30:00.55'),
+            dateEnd: new Date('2018-01-09T20:45:00.55')
+          }
+        ]
       },
       {
         title: 'Черная вдова',
         description: '3 - 6 человек',
+        events: [],
       },
       {
         title: 'Белорусский ликёр',
         description: '3 - 6 человек',
+        events: []
       },
     ]
   },
-  {
-    title: '7 этаж',
-    rooms: [
-      {
-        title: 'Джокер',
-        description: '3 - 6 человек',
-      },
-      {
-        title: 'Мариванна',
-        description: '3 - 6 человек',
-      },
-      {
-        title: 'Тонкий боб',
-        description: '3 - 6 человек',
-      },
-      {
-        title: 'Черная вдова',
-        description: '3 - 6 человек',
-      },
-      {
-        title: 'Белорусский ликёр',
-        description: '3 - 6 человек',
-      },
-    ]
-  },
-  {
-    title: '8 этаж',
-    rooms: [
-      {
-        title: 'Джокер',
-        description: '3 - 6 человек',
-      },
-      {
-        title: 'Мариванна',
-        description: '3 - 6 человек',
-      },
-      {
-        title: 'Тонкий боб',
-        description: '3 - 6 человек',
-      },
-      {
-        title: 'Черная вдова',
-        description: '3 - 6 человек',
-      },
-      {
-        title: 'Белорусский ликёр',
-        description: '3 - 6 человек',
-      },
-    ]
-  },
-  {
-    title: '9 этаж',
-    rooms: [
-      {
-        title: 'Джокер',
-        description: '3 - 6 человек',
-      },
-      {
-        title: 'Мариванна',
-        description: '3 - 6 человек',
-      },
-      {
-        title: 'Тонкий боб',
-        description: '3 - 6 человек',
-      },
-      {
-        title: 'Черная вдова',
-        description: '3 - 6 человек',
-      },
-      {
-        title: 'Белорусский ликёр',
-        description: '3 - 6 человек',
-      },
-    ]
-  }
 ];
 
 storiesOf('MainPage', module)
