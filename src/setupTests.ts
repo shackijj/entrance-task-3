ce9c57053e3b4f47af93eb31bd681179d60634dd
Kirill Shatskiy <shaackiy@gmail.com>
2018-01-12 15:16:32 +0300

    Updat setupTests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,6 @@
 import { configure } from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
+import * as moment from 'moment';
+moment.locale('ru');
 
 configure({ adapter: new Adapter() });
