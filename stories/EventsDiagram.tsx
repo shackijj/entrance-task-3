21562025041b24059d4c06e7b0ad129976be95f2
Kirill Shatskiy <shaackiy@gmail.com>
2018-01-21 19:43:10 +0300

    Update EventDiagram story and add snapshot test

--- stories/EventsDiagram.tsx
+++ stories/EventsDiagram.tsx
@@ -3,39 +3,35 @@ import EventsDiagram from '../src/components/EventsDiagram';
 import { storiesOf } from '@storybook/react';
 import './EventDiagram.css';
 
-const room = {
-  title: 'Ржавый Фред',
-  capacity: 7,
-  events: [
-    {
-      title: 'Событие 1',
-      dateStart: '2018-01-09T06:30:00.55',
-      dateEnd: '2018-01-09T08:45:00.55',
-      users: [],
-    },
-    {
-      title: 'Событие 2',
-      dateStart: '2018-01-09T09:30:00.55',
-      dateEnd: '2018-01-09T11:45:00.55',
-      users: [],
-    },
-    {
-      title: 'Событие 3',
-      dateStart: '2018-01-09T18:30:00.55',
-      dateEnd: '2018-01-09T20:45:00.55',
-      users: [],
-    }
-  ]
-};
-
-const lotsOfRooms = [];
-for (let i = 0; i < 100; i++) {
-  lotsOfRooms.push(room);
-}
 const floor = [
   {
     floor: 7,
-    rooms: lotsOfRooms
+    rooms: [
+      {
+        title: 'Ржавый Фред',
+        capacity: 7,
+        events: [
+          {
+            title: 'Событие 1',
+            dateStart: '2018-01-09T06:30:00.55',
+            dateEnd: '2018-01-09T08:45:00.55',
+            users: [],
+          },
+          {
+            title: 'Событие 2',
+            dateStart: '2018-01-09T09:30:00.55',
+            dateEnd: '2018-01-09T11:45:00.55',
+            users: [],
+          },
+          {
+            title: 'Событие 3',
+            dateStart: '2018-01-09T18:30:00.55',
+            dateEnd: '2018-01-09T20:45:00.55',
+            users: [],
+          }
+        ]
+      }
+    ]
   }
 ];
 
