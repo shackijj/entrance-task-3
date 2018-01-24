051860c2b723a8636e274027682405fb729ebd57
Kirill Shatskiy <shaackiy@gmail.com>
2018-01-24 09:57:16 +0300

    Fix storybook

--- stories/EventsDiagram.tsx
+++ stories/EventsDiagram.tsx
@@ -12,18 +12,21 @@ const floor = [
         capacity: 7,
         events: [
           {
+            id: '1',
             title: 'Событие 1',
             dateStart: '2018-01-09T06:30:00.55',
             dateEnd: '2018-01-09T08:45:00.55',
             users: [],
           },
           {
+            id: '1',
             title: 'Событие 2',
             dateStart: '2018-01-09T09:30:00.55',
             dateEnd: '2018-01-09T11:45:00.55',
             users: [],
           },
           {
+            id: '1',
             title: 'Событие 3',
             dateStart: '2018-01-09T18:30:00.55',
             dateEnd: '2018-01-09T20:45:00.55',
