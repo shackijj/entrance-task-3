b8d6d38783ec20a8b2d13a7b83d15cddacf1e303
Kirill Shatskiy <kshatskiy@Kirills-Mini.iponweb.lan>
2018-01-25 18:09:18 +0300

    Refactoring slots generation. Tests are broken

--- src/components/RoomTimeline.test.tsx
+++ src/components/RoomTimeline.test.tsx
@@ -1,314 +1,99 @@
 import * as React from 'react';
 import { shallow, mount } from 'enzyme';
-import RoomTimeline from './RoomTimeline';
+import RoomTimeline, { generateFreeSlots, generateSlots } from './RoomTimeline';
 
-describe('RoomTimeline', () => {
-  const dateCurrent = new Date('2018-01-09T07:30:00.55');
-  const users = [
-    {
-      login: 'Test',
-      avatarUrl: 'Fest'
-    }
-  ];
-  const room = {
-    title: 'Ржавый Фред',
-    description: '3 - 6 человек',
-    capacity: 6,
-    events: [
+const getDuration = (start: string, end: string) => (new Date(end)).getTime() - (new Date(start)).getTime();
+
+describe('#generateFreeSlots', () => {
+  it('should dateStart and dateEnd and generate free slots', () => {
+    const dateStart = '2018-01-09T06:33:00.000Z';
+    const dateEnd = '2018-01-09T08:28:00.000Z';
+
+    const event1Start = '2018-01-09T06:33:00.000Z';
+    const event1End = '2018-01-09T06:59:59.999Z';
+
+    const event2Start = '2018-01-09T07:00:00.000Z';
+    const event2End = '2018-01-09T07:59:59.999Z';
+
+    const event3Start = '2018-01-09T08:00:00.000Z';
+    const event3End = '2018-01-09T08:28:00.000Z';
+
+    const expected = [
+      {
+        type: 'free',
+        dateStart: event1Start,
+        dateEnd: event1End,
+        duration: getDuration(event1Start, event1End)
+      },
+      {
+        type: 'free',
+        dateStart: event2Start,
+        dateEnd: event2End,
+        duration: getDuration(event2Start, event2End)
+      },
+      {
+        type: 'free',
+        dateStart: event3Start,
+        dateEnd: event3End,
+        duration: getDuration(event3Start, event3End)
+      },
+    ];
+
+    const actual = generateFreeSlots(dateStart, dateEnd);
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe('#generateSlots', () => {
+  it('duration of the first event is calculated according to hourStart', () => {
+    const hourStart = 7;
+    const hourStartDate = '2018-01-09T07:00:00.000Z';
+    const hourEnd = 9;
+    const hourEndDate = '2018-01-09T09:59:59.999Z';
+
+    const eventStart = '2018-01-09T06:30:00.000Z';
+    const eventEnd = '2018-01-09T08:44:59.999Z';
+
+    const free1Start = '2018-01-09T08:45:00.000Z';
+    const free1End = '2018-01-09T08:59:59.999Z';
+
+    const free2Start = '2018-01-09T09:00:00.000Z';
+
+    const events = [
       {
         id: '1',
-        title: 'Событие 1',
-        dateStart: '2018-01-09T06:30:00.55',
-        dateEnd: '2018-01-09T08:45:00.55',
-        users
+        dateStart: eventStart,
+        dateEnd: eventEnd,
+      },
+    ];
+
+    const expected = [
+      {
+        type: 'event',
+        id: '1',
+        dateStart: eventStart,
+        dateEnd: eventEnd,
+        duration: getDuration(hourStartDate, eventEnd)
       },
       {
-        id: '2',
-        title: 'Событие 2',
-        dateStart: '2018-01-09T09:30:00.55',
-        dateEnd: '2018-01-09T11:45:00.55',
-        users
+        type: 'free',
+        dateStart: free1Start,
+        dateEnd: free1End,
+        duration: getDuration(free1Start, free1End)
       },
       {
-        id: '3',
-        title: 'Событие 3',
-        dateStart: '2018-01-09T18:30:00.55',
-        dateEnd: '2018-01-09T20:45:00.55',
-        users
-      }
-    ]
-  };
-
-  const getStyleProp = (element: React.ReactElement<{}>) => shallow(element).prop('style');
-
-  it('should be a room representation for the timeline', () => {
-    const wrapper = shallow(
-      <RoomTimeline
-        {...room}
-        dateCurrent={dateCurrent}
-        hourStart={0}
-        hourEnd={23}
-      />
-    );
-    expect(wrapper.find('.RoomTimeline')).toHaveLength(1);
+        type: 'free',
+        dateStart: free2Start,
+        dateEnd: hourEndDate,
+        duration: getDuration(free2Start, hourEndDate)
+      },
+    ];
+    const actual = generateSlots(events, hourStart, hourEnd);
+    expect(actual).toEqual(expected);
   });
+});
 
-  it('should render slots for timeline view', () => {
-    const wrapper = shallow(
-      <RoomTimeline
-        {...room}
-        dateCurrent={dateCurrent}
-        hourStart={0}
-        hourEnd={23}
-      />
-    );
-    expect(wrapper.find('.RoomTimeline-Slot')).toHaveLength(7);
-    expect(wrapper.find('.RoomTimeline-Slot_past')).toHaveLength(1);
-    expect(wrapper.find('.RoomTimeline-Slot_free')).toHaveLength(3);
-    expect(wrapper.find('.RoomTimeline-Slot_event')).toHaveLength(3);
-  });
-
-  it('should work without dateCurrent', () => {
-    const wrapper = shallow(
-      <RoomTimeline
-        {...room}
-        hourStart={0}
-        hourEnd={23}
-      />
-    );
-
-    expect(wrapper.find('.RoomTimeline-Slot')).toHaveLength(7);
-    expect(wrapper.find('.RoomTimeline-Slot_free')).toHaveLength(4);
-    expect(wrapper.find('.RoomTimeline-Slot_event')).toHaveLength(3);
-  });
-
-  it('should set width in % for each slot', () => {
-    const wrapper = shallow(
-      <RoomTimeline
-        {...room}
-        hourStart={0}
-        hourEnd={23}
-      />
-    );
-    
-    const slots = wrapper.find('.RoomTimeline-Slot');
-    expect(slots).toHaveLength(7);
-    expect(getStyleProp(slots.get(0))).toEqual({
-      width: '27.083333%' // 6h30m / 24h
-    });
-    expect(getStyleProp(slots.get(1))).toEqual({
-      width: '9.375000%' // 2h15m / 24h
-    });
-    expect(getStyleProp(slots.get(2))).toEqual({
-      width: '3.125000%' // 45m / 24h
-    });
-    expect(getStyleProp(slots.get(3))).toEqual({
-      width: '9.375000%' // 2h15m / 24h
-    });
-    expect(getStyleProp(slots.get(4))).toEqual({
-      width: '28.125000%' // 2h15m / 24h
-    });
-    expect(getStyleProp(slots.get(5))).toEqual({
-      width: '9.375000%' // 2h15m / 24h
-    });
-    expect(getStyleProp(slots.get(6))).toEqual({
-      width: '13.541667%' // 3h15m / 24h
-    });
-  });
-
-  it('should be ok with other hourStart and hourEnd', () => {
-    const wrapper = shallow(
-      <RoomTimeline
-        {...room}
-        hourStart={7}
-        hourEnd={23}
-      />
-    );
-
-    const slots = wrapper.find('.RoomTimeline-Slot');
-    expect(slots).toHaveLength(6);
-    expect(getStyleProp(slots.get(0))).toEqual({
-      width: '10.294118%' // 6h30m / 17h
-    });
-    expect(getStyleProp(slots.get(1))).toEqual({
-      width: '4.411765%' // 2h15m / 17h
-    });
-    expect(getStyleProp(slots.get(2))).toEqual({
-      width: '13.235294%' // 45m / 17h
-    });
-    expect(getStyleProp(slots.get(3))).toEqual({
-      width: '39.705882%' // 2h15m / 17h
-    });
-    expect(getStyleProp(slots.get(4))).toEqual({
-      width: '13.235294%' // 2h15m / 17h
-    });
-    expect(getStyleProp(slots.get(5))).toEqual({
-      width: '19.117647%' // 3h15m / 17h
-    });
-  });
-
-  it('should set width in % for each slot when dateCurrent given', () => {
-    const wrapper = shallow(
-      <RoomTimeline
-        dateCurrent={new Date('2018-01-09T07:30:00.55')}
-        hourStart={0}
-        hourEnd={23}
-        title={'Ржавый Фред'}
-        capacity={6}
-        events={
-          [
-            {
-              id: '1',
-              dateStart: '2018-01-09T06:30:00.55',
-              dateEnd: '2018-01-09T08:45:00.55',
-            },
-          ]
-        }
-      />
-    );
-
-    const slots = wrapper.find('.RoomTimeline-Slot');
-    expect(getStyleProp(slots.get(0))).toEqual({
-      width: '27.083333%' // 6h30m / 24h
-    });
-    expect(getStyleProp(slots.get(1))).toEqual({
-      width: '9.375000%' // 2h15m / 24h
-    });
-    expect(getStyleProp(slots.get(2))).toEqual({
-      width: '63.541667%' // 15h15m / 24h
-    });
-  });
-
-  it('the first event is earlier that dateCurrent', () => {
-    const wrapper = shallow(
-      <RoomTimeline
-        dateCurrent={new Date('2018-01-09T07:30:00.55')}
-        hourStart={0}
-        hourEnd={23}
-        title={'Ржавый Фред'}
-        capacity={6}
-        events={
-          [
-            {
-              id: '1',
-              dateStart: '2018-01-09T05:04:00.55',
-              dateEnd: '2018-01-09T10:00:00.23',
-            },
-          ]
-        }
-      />
-    );
-
-    const slots = wrapper.find('.RoomTimeline-Slot');
-    expect(getStyleProp(slots.get(0))).toEqual({
-      width: '21.111111%' // 5h4m / 24h
-    });
-    expect(getStyleProp(slots.get(1))).toEqual({
-      width: '20.555556%' // 4h56m / 24h
-    });
-    expect(getStyleProp(slots.get(2))).toEqual({
-      width: '58.333333%' // 14h0m / 24h
-    });
-  });
-
-  it('should render one free slot if events are empty', () => {
-    const wrapper = shallow(
-      <RoomTimeline
-        hourStart={0}
-        hourEnd={23}
-        title={'Ржавый Фред'}
-        capacity={4}
-        events={[]}
-      />
-    );
-
-    const slots = wrapper.find('.RoomTimeline-Slot');
-    expect(slots).toHaveLength(1);
-    expect(getStyleProp(slots.get(0))).toEqual({
-      width: '100.000000%'
-    });
-  });
-
-  it('should render one free slot if events are empty', () => {
-    const wrapper = shallow(
-      <RoomTimeline
-        dateCurrent={new Date('2018-01-09T07:30:00.55')}
-        hourStart={0}
-        hourEnd={23}
-        title={'Ржавый Фред'}
-        capacity={4}
-        events={[]}
-      />
-    );
-
-    const slots = wrapper.find('.RoomTimeline-Slot');
-    expect(slots).toHaveLength(2);
-    expect(getStyleProp(slots.get(0))).toEqual({
-      width: '31.250000%'
-    });
-    expect(getStyleProp(slots.get(1))).toEqual({
-      width: '68.750000%'
-    });
-  });
-
-  it('empty events and current date width non-default start hours', () => {
-    const wrapper = shallow(
-      <RoomTimeline
-        dateCurrent={new Date('2018-01-09T07:30:00.55')}
-        hourStart={7}
-        hourEnd={23}
-        title={'Ржавый Фред'}
-        capacity={4}
-        events={[]}
-      />
-    );
-
-    const slots = wrapper.find('.RoomTimeline-Slot');
-    expect(slots).toHaveLength(2);
-    expect(getStyleProp(slots.get(0))).toEqual({
-      width: '2.941176%'
-    });
-    expect(getStyleProp(slots.get(1))).toEqual({
-      width: '97.058824%'
-    });
-  });
-
-  it('empty events and current date width non-default start hours', () => {
-    const wrapper = shallow(
-      <RoomTimeline
-        dateCurrent={new Date('2018-01-09T07:30:00.55')}
-        hourStart={7}
-        hourEnd={23}
-        title={'Ржавый Фред'}
-        capacity={4}
-        events={
-          [
-            {
-              id: '1',
-              dateStart: '2018-01-09T10:00:00.55',
-              dateEnd: '2018-01-09T13:00:00.55',
-            },
-          ]
-        }
-      />
-    );
-
-    const slots = wrapper.find('.RoomTimeline-Slot');
-    expect(slots).toHaveLength(4);
-    expect(getStyleProp(slots.get(0))).toEqual({
-      width: '2.941176%'
-    });
-    expect(getStyleProp(slots.get(1))).toEqual({
-      width: '14.705882%'
-    });
-    expect(getStyleProp(slots.get(2))).toEqual({
-      width: '17.647059%'
-    });
-    expect(getStyleProp(slots.get(3))).toEqual({
-      width: '64.705882%'
-    });
-  });
-
+describe('RoomTimeline', () => {
   it('should fire onEventClick when an event is clicked', () => {
     const spy = jest.fn();
     const wrapper = mount(
--- src/components/RoomTimeline.tsx
+++ src/components/RoomTimeline.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './RoomTimeline.css';
 import { RoomProps } from './Room';
+import * as moment from 'moment';
 
 interface Event {
   id: string;
@@ -19,12 +20,121 @@ export interface RoomTimelineProps extends RoomProps {
 
 const getMinutes = (date: Date) => date.getHours() * 60 + date.getMinutes();
 
+export const generateFreeSlots = (dateStart: string, dateEnd: string) => {
+  const rc: Array<{type: 'free', dateStart: string, dateEnd: string, duration: number}> = [];
+  let start = moment(dateStart);
+  let end = moment(dateEnd);
+
+  let nextWholeHour = start.clone().endOf('hour');
+
+  while (nextWholeHour < end) {
+    rc.push({
+      type: 'free',
+      dateStart: start.toISOString(),
+      dateEnd: nextWholeHour.toISOString(),
+      duration: nextWholeHour.valueOf() - start.valueOf(),
+    });
+
+    start = nextWholeHour.clone().add(1, 'hour').startOf('hour');
+    nextWholeHour = nextWholeHour.clone().add(1, 'hours').endOf('hour');
+  }
+  rc.push({
+    type: 'free',
+    dateStart: start.toISOString(),
+    dateEnd,
+    duration: moment(dateEnd).valueOf() - start.valueOf()
+  });
+
+  return rc;
+};
+
+export const generateSlots = (events: Event[], hourStart: number, hourEnd: number, dateCurrent?: Date) => {
+  const slotProps: Array<{duration: number, type: string, id?: string}> = [];
+
+  let prevEvent: Event | undefined;
+  let length = events.length;
+
+  events.forEach((curEvent, idx, arr) => {
+    const {id} = curEvent;
+    const dateStart = new Date(curEvent.dateStart);
+    const dateEnd = new Date(curEvent.dateEnd);
+    let startOfDay = moment(dateStart).startOf('day');
+
+    if (idx === 0 && hourStart <= dateStart.getHours()) {
+      if (dateCurrent && dateCurrent < dateStart) {
+        slotProps.push({
+          duration: getMinutes(dateCurrent) - hourStart * 60,
+          type: 'past'
+        });
+        generateFreeSlots(dateCurrent.toISOString(), dateStart.toISOString())
+          .map((slot) => slotProps.push(slot));
+      } else {
+        if (dateCurrent) {
+          slotProps.push({
+            duration: (dateStart.getHours() - hourStart) * 60 + dateStart.getMinutes(),
+            type: 'past',
+          });
+        } else {
+          generateFreeSlots(startOfDay.clone().add(hourStart, 'hours').toISOString(), dateStart.toISOString())
+            .map((slot) => slotProps.push(slot));
+        }
+      }
+    }
+
+    if (prevEvent && new Date(prevEvent.dateEnd) < dateStart) {
+      generateFreeSlots(prevEvent.dateEnd, dateStart.toISOString())
+        .map((slot) => slotProps.push(slot));
+    }
+
+    if (hourStart > dateStart.getHours()) {
+      slotProps.push({
+        duration: getMinutes(dateEnd) - (hourStart * 60),
+        id,
+        type: 'event',
+      });
+    } else {
+      slotProps.push({
+        duration: getMinutes(dateEnd) - getMinutes(dateStart),
+        id,
+        type: 'event',
+      });
+    }
+
+    if (idx === length - 1 && dateEnd.getHours() < hourEnd) {
+      generateFreeSlots(
+        dateEnd.toISOString(),
+        startOfDay.clone().add(hourEnd, 'hours').add(59, 'minutes').toISOString())
+        .map((slot) => slotProps.push(slot));
+    }
+    prevEvent = curEvent;
+  });
+
+  if (events.length === 0) {
+    if (dateCurrent) {
+      let startOfDay = moment(dateCurrent).startOf('day');
+      const pastMinues = getMinutes(dateCurrent) - hourStart * 60;
+      slotProps.push({
+        duration: pastMinues,
+        type: 'past',
+      });
+      generateFreeSlots(
+        dateCurrent.toISOString(),
+        startOfDay.clone().add(hourEnd, 'hours').add(59, 'minutes').toISOString())
+        .map((slot) => slotProps.push(slot));
+    } else {
+      let startOfDay = moment(new Date()).startOf('day');
+      generateFreeSlots(
+        startOfDay.clone().add(hourStart, 'hours').toISOString(),
+        startOfDay.clone().add(hourEnd, 'hours').add(59, 'minutes').toISOString())
+        .map((slot) => slotProps.push(slot));
+    }
+  }
+  return slotProps;
+};
+
 const RoomTimeline: React.SFC<RoomTimelineProps> =
   ({dateCurrent, events, hourStart, hourEnd, title, onEventClick}) => {
-    const slotProps: Array<{duration: number, type: string, id?: string}> = [];
-
-    let prevEvent: Event | undefined;
-    let length = events.length;
+    const slotProps = generateSlots(events, hourStart, hourEnd, dateCurrent);
 
     const totalMinutes = (hourEnd - hourStart + 1) * 60;
 
@@ -36,78 +146,6 @@ const RoomTimeline: React.SFC<RoomTimelineProps> =
         }
     };
 
-    events.forEach((curEvent, idx, arr) => {
-      const {id} = curEvent;
-      const dateStart = new Date(curEvent.dateStart);
-      const dateEnd = new Date(curEvent.dateEnd);
-
-      if (idx === 0 && hourStart <= dateStart.getHours()) {
-        if (dateCurrent && dateCurrent < dateStart) {
-          slotProps.push({
-            duration: getMinutes(dateCurrent) - hourStart * 60,
-            type: 'past'
-          });
-          slotProps.push({
-            duration: getMinutes(dateStart) - getMinutes(dateCurrent),
-            type: 'free'
-          });
-        } else {
-          slotProps.push({
-            duration: (dateStart.getHours() - hourStart) * 60 + dateStart.getMinutes(),
-            type: dateCurrent ? 'past' : 'free'
-          });
-        }
-      }
-
-      if (prevEvent && new Date(prevEvent.dateEnd) < dateStart) {
-        slotProps.push({
-          duration: getMinutes(dateStart) - getMinutes(new Date(prevEvent.dateEnd)),
-          type: 'free'
-        });
-      }
-
-      if (hourStart > dateStart.getHours()) {
-        slotProps.push({
-          duration: getMinutes(dateEnd) - (hourStart * 60),
-          id,
-          type: 'event',
-        });
-      } else {
-        slotProps.push({
-          duration: getMinutes(dateEnd) - getMinutes(dateStart),
-          id,
-          type: 'event',
-        });
-      }
-
-      if (idx === length - 1 && dateEnd.getHours() < hourEnd) {
-        slotProps.push({
-          duration: totalMinutes - getMinutes(dateEnd) + hourStart * 60,
-          type: 'free'
-        });
-      }
-      prevEvent = curEvent;
-    });
-
-    if (events.length === 0) {
-      if (dateCurrent) {
-        const pastMinues = getMinutes(dateCurrent) - hourStart * 60;
-        slotProps.push({
-          duration: pastMinues,
-          type: 'past',
-        });
-        slotProps.push({
-          duration: totalMinutes - pastMinues,
-          type: 'free',
-        });
-      } else {
-        slotProps.push({
-          duration: totalMinutes,
-          type: 'free',
-        });
-      }
-    }
-
     return (
       <div className="RoomTimeline" onClick={onClick}>
         {slotProps.map(({type, duration, id}, idx) => (
@@ -115,7 +153,7 @@ const RoomTimeline: React.SFC<RoomTimelineProps> =
             key={idx}
             className={`RoomTimeline-Slot RoomTimeline-Slot_${type}`}
             data-event-id={id}
-            style={{width: `${((duration * 100 / totalMinutes).toFixed(6))}%`}}
+            style={{width: `${(duration * 100 / totalMinutes)}%`}}
           />
         ))}
       </div>
