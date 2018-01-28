8d48934652c91ab7df121e5ceb94c41034fd9d02
Kirill Shatskiy <shaackiy@gmail.com>
2018-01-28 11:59:36 +0300

    Fix story. Update tsconfigs

--- stories/EventsDiagram.tsx
+++ stories/EventsDiagram.tsx
@@ -8,30 +8,17 @@ const floor = [
     floor: 7,
     rooms: [
       {
+        id: '1',
         title: 'Ржавый Фред',
         capacity: 7,
         events: [
           {
             id: '1',
             title: 'Событие 1',
-            dateStart: '2018-01-09T06:30:00.55',
+            dateStart: '2018-01-09T08:30:00.55',
             dateEnd: '2018-01-09T08:45:00.55',
             users: [],
           },
-          {
-            id: '1',
-            title: 'Событие 2',
-            dateStart: '2018-01-09T09:30:00.55',
-            dateEnd: '2018-01-09T11:45:00.55',
-            users: [],
-          },
-          {
-            id: '1',
-            title: 'Событие 3',
-            dateStart: '2018-01-09T18:30:00.55',
-            dateEnd: '2018-01-09T20:45:00.55',
-            users: [],
-          }
         ]
       }
     ]
