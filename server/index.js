b7f6220ca51f099130f4eba2be06c8c4a8806467
Kirill Shatskiy <shaackiy@gmail.com>
2018-01-30 08:16:25 +0300

    Remove server files

--- server/index.js
+++
@@ -1,7 +0,0 @@
-const { start } = require('./src/server')
-
-const PORT = 4000
-
-start(PORT).then(({app}) => {
-  console.log(`Graphql server is listening on http://localhost:${PORT}`)
-})
--- server/src/schema/index.js
+++
@@ -1,9 +0,0 @@
-const typeDefs = require('./typeDefs')
-const resolvers = require('./resolvers')
-
-const { makeExecutableSchema } = require('graphql-tools')
-
-module.exports = makeExecutableSchema({
-  typeDefs,
-  resolvers: resolvers()
-})
